8fee02904de0f4541422ded94f589b8c1f9aed9f
MrCalamitus <calamitus@hotmail.es>
2025-04-10 14:02:21 -0600

    Enhance error handling in GlobalMethods class for ASN1 decoding and update PrivateKey class to use 'readonly' for properties. Adjusted formatting for consistency and improved SonarQube exclusions in sonar-project.properties.

--- lib/PrivateKey.d.ts
+++ lib/PrivateKey.d.ts
@@ -1,7 +1,7 @@
 export declare class PrivateKey {
-    private asn1Object;
-    private encryptedPrivateKeyValidator;
+    private readonly asn1Object;
+    private readonly encryptedPrivateKeyValidator;
     constructor(x509Binary: string);
     rsaDecrypt(encryptedText: string, passwordKey: string): string;
-    rsaSign(message: string, passwordKey: string, encoding?: 'utf8'): string;
+    rsaSign(message: string, passwordKey: string, encoding?: "utf8"): string;
 }
